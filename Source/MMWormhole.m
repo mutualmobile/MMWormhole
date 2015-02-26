cd8f2c7c74ad22d17f2bb7c90837de87d7a215c4
felixLam <felix@iosphere.de>
2015-02-26 20:10:08 +0100

    Removes whitespace

--- Source/MMWormhole.m
+++ Source/MMWormhole.m
@@ -159,7 +159,7 @@ static NSString * const MMWormholeNotificationName = @"MMWormholeNotificationNam
     [[self class] sendNotificationForMessageWithIdentifier:identifier];
 }
 
-+ (void)sendNotificationForMessageWithIdentifier:(NSString *)identifier  {
++ (void)sendNotificationForMessageWithIdentifier:(NSString *)identifier {
     CFNotificationCenterRef const center = CFNotificationCenterGetDarwinNotifyCenter();
     CFDictionaryRef const userInfo = NULL;
     BOOL const deliverImmediately = YES;
