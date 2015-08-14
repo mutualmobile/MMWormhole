fe3621323b567c66852b0071cc6534375e1fa641
Thomas Guthrie <tomguthrie@gmail.com>
2015-08-14 11:34:51 +0100

    Add custom module.modulemap

---
+++ MMWormhole/MMWormhole/MMWormholeUmbrella.h
@@ -0,0 +1,13 @@
+//
+//  MMWormhole.h
+//  MMWormhole
+//
+//  Created by LEI on 5/11/15.
+//  Copyright (c) 2015 MMWormhole. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <MMWormhole/MMWormhole.h>
+#import <MMWormhole/MMWormholeFileTransiting.h>
+#import <MMWormhole/MMWormholeCoordinatedFileTransiting.h>
