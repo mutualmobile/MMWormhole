b95f2aaa2526b0b661bd2ba91dc397210d1e9b41
Conrad Stoll <conrad.stoll@mutualmobile.com>
2015-09-13 13:09:29 -0500

    Updating documentation

--- Source/MMWormholeSessionFileTransiting.h
+++ Source/MMWormholeSessionFileTransiting.h
@@ -23,6 +23,25 @@
 
 #import "MMWormholeFileTransiting.h"
 
+/**
+ This class provides support for the WatchConnectivity framework's file transfer ability. This class 
+ will behave very similar to the MMWormholeFileTransiting implementation, meaning it will archive
+ messages to disk as files and send them via the WatchConnectivity framework's -transferFile API.
+ 
+ @warning In order for your Wormhole to support reading the contents of transferred file messages,
+ you will need to set this object as the 'wormholeMessenger' property on the
+ [MMWormholeSession sharedListeningSession]. The reason for this is that the sharedListeningSession
+ requires a configured application group and optional file directory in order to know where to save
+ received files. If you don't set this object as the wormholeMessenger you will still be notified
+ when your receive files, but you will be responsible for storing the contents yourself and they
+ won't be persisted for you.
+ 
+ @discussion This class should only be used in very specific circumstances. Typically speaking, if
+ you find yourself needing to use the WatchConnectivity framework's file transfer APIs you will best
+ be served by using the WatchConnectivity framework directly and bypassing MMWormhole. This class
+ is provided as a basic implementation for simple use cases and isn't intended to be the core of
+ your file based message transfer system.
+ */
 @interface MMWormholeSessionFileTransiting : MMWormholeFileTransiting
 
 @end
