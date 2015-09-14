6c13c2718244d04bfa8fe23da16912763bff3f2f
Conrad Stoll <conrad.stoll@mutualmobile.com>
2015-09-14 17:25:26 -0500

    Adding Carthage Support to new project files

--- MMWormhole/MMWormhole/MMWormholeUmbrella.h
+++ MMWormhole/MMWormhole/MMWormholeUmbrella.h
@@ -17,3 +17,12 @@ FOUNDATION_EXPORT const unsigned char MMWormholeVersionString[];
 #import <MMWormhole/MMWormhole.h>
 #import <MMWormhole/MMWormholeFileTransiting.h>
 #import <MMWormhole/MMWormholeCoordinatedFileTransiting.h>
+
+#if ( ( defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 90000 ) || TARGET_OS_WATCH )
+#import <MMWormhole/MMWormholeSession.h>
+#import <MMWormhole/MMWormholeSessionContextTransiting.h>
+#import <MMWormhole/MMWormholeSessionFileTransiting.h>
+#import <MMWormhole/MMWormholeSessionMessageTransiting.h>
+#endif
+
+#import <MMWormhole/MMWormholeTransiting.h>
