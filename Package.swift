8234a380d39c25d1c9f4575c34da89d6280e6fc1
Eli Gutovsky <elgutovsky@deloitte.co.il>
2023-09-11 15:07:52 +0300

    feat: add the package description

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "MMWormhole",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v11),
+        .watchOS(.v2),
+    ],
+    products: [
+        .library(
+            name: "MMWormhole",
+            targets: ["MMWormhole"]),
+    ],
+    targets: [
+        .target(
+            name: "MMWormhole",
+            path: "Source",
+            publicHeadersPath: "."),
+    ])
